ea969ddb3f713cae5d50de31995f385d3d66078c
Esha Patel <Patellesha11@gmail.com>
2025-04-13 22:43:50 +0300

    Add question and answer to faqData in Accordion component

--- src/Components/Accordion.js
+++ src/Components/Accordion.js
@@ -2,3 +2,5 @@ import { useState } from "react";
 
 const faqData = [
     {
+         Question: "What is the only country that spans two continents?",
+         Answer: "Turkey (Europe and Asia)".at,
